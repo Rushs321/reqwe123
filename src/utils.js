d1c2c12ae7082a74872fb4ee6450ef0b6c1dd963
Rushs321 <fyggh566@tutamail.com>
2024-08-24 21:10:59 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-
+"use strict";
 function generateRandomIP() {
   return `${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}`;
 }
